c33dd6fef0d7070a14b1ca994012f2d97096fbed
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-08-25 00:46:18 +0300

    Footer part is done

--- src/components/EnterEmail.tsx
+++ src/components/EnterEmail.tsx
@@ -1,18 +1,30 @@
 "use client";
+import { useForm, SubmitHandler, FieldValues } from "react-hook-form";
 
 const EnterEmail = () => {
+  const { register, handleSubmit } = useForm<FieldValues>({
+    defaultValues: { email: "" },
+  });
+  const onSubmit: SubmitHandler<FieldValues> = (data) => console.log(data);
+
   return (
-    <div className="w-[377px] h-11 bg-white rounded-[3px] relative p-[2px] flex justify-between items-center">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="w-[377px] h-11 bg-white rounded-[3px] relative p-[2px] flex justify-between items-center"
+    >
       <input
         className=" text-base font-normal pl-5 py-[11px]"
         type="email"
         placeholder="Enter Email Address"
-        onChange={() => {}}
+        {...register("email", { required: true })}
       />
-      <button className="w-[135px] h-full bg-pink-500 rounded-[3px] flex justify-center items-center text-indigo-50 text-base font-medium" onClick={() => {}}>
+      <button
+        className="w-[135px] h-full bg-pink-500 rounded-[3px] flex justify-center items-center text-indigo-50 text-base font-medium"
+        type="submit"
+      >
         Sign Up
       </button>
-    </div>
+    </form>
   );
 };
 
